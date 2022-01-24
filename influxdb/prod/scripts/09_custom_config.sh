b307d5033a8d8a5213a7f987b8c393144abf78d0
Vincent Gourrierec <vincent@lighton.ai>
2022-01-24 11:57:34 +0100

    Corrections to custom config script.

--- influxdb/prod/scripts/09_custom_config.sh
+++ influxdb/prod/scripts/09_custom_config.sh
@@ -5,10 +5,8 @@ RETENTION_POLICY="90d"
 BUCKET_NAME="muse_metrics"
 TOKEN=$DOCKER_INFLUXDB_INIT_ADMIN_TOKEN
 ORG=$DOCKER_INFLUXDB_INIT_ORG
-
-BUCKET_ID=$(influx bucket list -n $BUCKET_NAME --hide-headers --org $ORG --token $TOKEN | awk '{print $1}')
-
 influx bucket create -n $BUCKET_NAME -r $RETENTION_POLICY --org $ORG --token $TOKEN
+BUCKET_ID=$(influx bucket list -n $BUCKET_NAME --hide-headers --org $ORG --token $TOKEN | awk '{print $1}')
 influx v1 dbrp create --db $BUCKET_NAME --rp $RETENTION_POLICY --bucket-id $BUCKET_ID --default --org $ORG --token $TOKEN
 
 
@@ -16,8 +14,6 @@ RETENTION_POLICY="30d"
 BUCKET_NAME="muse_system_metrics"
 TOKEN=$DOCKER_INFLUXDB_INIT_ADMIN_TOKEN
 ORG=$DOCKER_INFLUXDB_INIT_ORG
-
-BUCKET_ID=$(influx bucket list -n $BUCKET_NAME --hide-headers --org $ORG --token $TOKEN | awk '{print $1}')
-
 influx bucket create -n $BUCKET_NAME -r $RETENTION_POLICY --org $ORG --token $TOKEN
+BUCKET_ID=$(influx bucket list -n $BUCKET_NAME --hide-headers --org $ORG --token $TOKEN | awk '{print $1}')
 influx v1 dbrp create --db $BUCKET_NAME --rp $RETENTION_POLICY --bucket-id $BUCKET_ID --default --org $ORG --token $TOKEN
