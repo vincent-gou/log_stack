e88d0e130bcde8f6f6328d911ec572e8f65d9a11
Vincent Gourrierec <vincent@lighton.ai>
2022-01-24 12:04:25 +0100

    Corrections #3 to custom config script.

--- influxdb/prod/scripts/01_create_bucket.sh
+++ influxdb/prod/scripts/01_create_bucket.sh
@@ -5,3 +5,6 @@ influx bucket create -n loki_metrics -r 7d
 influx bucket create -n influxdb_metrics -r 7d
 influx bucket create -n grafana_metrics -r 7d
 #influx config ls
+
+influx bucket create -n muse_metrics -r 90d
+influx bucket create -n muse_system_metrics -r 30d
