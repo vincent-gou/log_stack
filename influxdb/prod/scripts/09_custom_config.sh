54e3050ea72d4dc4722275bd1da2aa39fd8d9ae7
Vincent Gourrierec <vincent@lighton.ai>
2022-01-24 11:49:47 +0100

    Add custom config script.

---
+++ influxdb/prod/scripts/09_custom_config.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+RETENTION_POLICY="90d"
+BUCKET_NAME="muse_metrics"
+TOKEN=$DOCKER_INFLUXDB_INIT_ADMIN_TOKEN
+ORG=$DOCKER_INFLUXDB_INIT_ORG
+
+BUCKET_ID=$(influx bucket list -n $BUCKET_NAME --hide-headers --org $ORG --token $TOKEN | awk '{print $1}')
+
+influx bucket create -n $BUCKET_NAME -r $RETENTION_POLICY --org $ORG --token $TOKEN
+influx v1 dbrp create --db $BUCKET_NAME --rp $RETENTION_POLICY --bucket-id $BUCKET_ID --default --org $ORG --token $TOKEN
+
+
+RETENTION_POLICY="30d"
+BUCKET_NAME="muse_system_metrics"
+TOKEN=$DOCKER_INFLUXDB_INIT_ADMIN_TOKEN
+ORG=$DOCKER_INFLUXDB_INIT_ORG
+
+BUCKET_ID=$(influx bucket list -n $BUCKET_NAME --hide-headers --org $ORG --token $TOKEN | awk '{print $1}')
+
+influx bucket create -n $BUCKET_NAME -r $RETENTION_POLICY --org $ORG --token $TOKEN
+influx v1 dbrp create --db $BUCKET_NAME --rp $RETENTION_POLICY --bucket-id $BUCKET_ID --default --org $ORG --token $TOKEN
